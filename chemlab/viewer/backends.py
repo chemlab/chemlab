27ff231fdc2e376d71f13522a430c5b6025f1ea5
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2012-02-08 11:39:04 +0100

    made a test for nose to display a molecule and made it run

---
+++ chemlab/viewer/backends.py
@@ -0,0 +1,27 @@
+"""Module containing interfaces with various GUI frameworks, now only the
+GLUT backend is supported.
+
+"""
+
+from OpenGL.GLUT import *
+
+class GLUTBackend(object):
+    def __init__(self):
+        glutInit(sys.argv);
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+        glutInitWindowSize(500, 600);
+        glutInitWindowPosition(50,50);
+        glutCreateWindow(sys.argv[0]);
+        glutDisplayFunc(self.display)
+        glutMotionFunc(self.move)
+        glutMouseFunc(self.wheel)
+
+    def refresh(self):
+        glutPostRedisplay()
+    
+    def main(self):
+        glutMainLoop()
+    
+    def swap_buffers(self):
+        glutSwapBuffers()
+
