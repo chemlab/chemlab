d73d598b62df2463bf35ac22cad498fbf2b8525b
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2012-02-08 11:32:30 +0100

    added interactive tests for the molecular viewer

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -3,6 +3,8 @@ they require interaction with the user.
 
 """
 import chemlab as cl
+from mock import Mock
+import numpy as np
 
 def test_read_and_display():
     """Read a molecule from disk and display it with the molecular
@@ -12,3 +14,34 @@ def test_read_and_display():
 
     mol = cl.read_molecule("testmol.xyz", format="tinker")
     cl.display(mol)
+
+def test_display_molecule():
+    """Display a molecule in the viewer using the viewer interface"""
+
+    mol = Mock()
+
+    at_params = [("C", (0.0, 0.0, 1.0)),
+                 ("C", (0.0, 1.0, 0.0)),
+                 ("H", (1.0, 0.0, 0.0)),
+                 ("O", (1.0, 1.0, 1.0))]
+    
+    mol.atoms = []
+    for sym, pos in at_params:
+        at = Mock()
+        at.coord = np.array(pos)
+        at.type = sym
+        
+        mol.atoms.append(at)
+
+    bond_params = [(0, 1), (1,2), (2,3)]
+    mol.bonds = []
+    for s,e in bond_params:
+        bond = Mock()
+        bond.start = mol.atoms[s]
+        bond.end = mol.atoms[e]
+        
+        mol.bonds.append(bond)
+
+    vw = cl.Viewer()
+    vw.draw_molecule(mol)
+    vw.show()
