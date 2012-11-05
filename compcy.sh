a50e3dd9f9a37963a5874ce810f24f3b067d9308
Gabriele Lanaro <gabriele@gabriele-desktop.(none)>
2012-11-05 14:10:54 -0800

    Added lm compilation

--- compcy.sh
+++ compcy.sh
@@ -3,7 +3,7 @@
 cython -a $1
 fname="${1%.*}"
 
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing \
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -lm -fno-strict-aliasing \
       -I/usr/include/python2.7 -o $fname.so $fname.c
 
 
