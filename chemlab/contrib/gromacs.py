66eec821449e99f7210b176c7f35a58921249e80
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2013-01-08 15:34:15 -0800

    [maybe] mini units package for chemlab to ease the conversion of units

---
+++ chemlab/contrib/gromacs.py
@@ -0,0 +1,6 @@
+'''A set of utilities to interact with gromacs'''
+
+# Need to add a parser to insert this contrib script
+
+# $ chemlab gromacs energy
+# it should show a little interface to view the energy
