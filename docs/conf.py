9edf5b505d3f8824b1343e2634273831eddbd72f
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2013-03-24 21:49:00 -0700

    trying to fix rtd

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ class Mock(object):
     
 MOCK_MODULES = ['PySide.QtGui', 'PySide.QtCore', 'PySide', 'PySide.QtOpenGL',
                 'chemlab.molsim.cforces2', 'h5py', 'chemlab.libs.pyxdr',
-                'Cython.Distutils', 'numpy', 'PyOpenGL']
+                'Cython.Distutils', 'numpy', 'numpy.linalg', 'PyOpenGL']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
