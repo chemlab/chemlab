dc88883ea18ae98b3cec754f82555a9f371fad59
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2013-05-02 14:58:47 -0700

    minimum image thing and translate option to the chemlab script

--- chemlab/utils/__init__.py
+++ chemlab/utils/__init__.py
@@ -5,3 +5,23 @@ from distances import overlapping_points
 
 def fequal(a, b, tol):
     return (abs(a-b) / max(abs(a), abs(b))) < tol
+
+def minimum_image(coords, pbc):
+    """
+    Wraps a vector collection of atom positions into the central periodic
+    image or primary simulation cell.
+    
+    Parameters
+    ----------
+    pos : :class:`numpy.ndarray`, (Nx3)
+    Vector collection of atom positions.
+    
+    Returns
+    -------
+    wrap : :class:`numpy.ndarray`, (Nx3)
+    Returns atomic positions wrapped into the primary simulation
+    cell, or periodic image.
+    
+    """
+    wrap = coords + (coords < 0.0)*pbc - (coords > pbc)*pbc
+    return wrap
