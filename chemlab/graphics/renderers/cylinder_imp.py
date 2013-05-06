41d175b082e3348d454614ed3d29dd67c3516414
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2013-05-06 10:08:22 -0700

    Saving progress on cylinder impostors

--- chemlab/graphics/renderers/cylinder_imp.py
+++ chemlab/graphics/renderers/cylinder_imp.py
@@ -28,32 +28,35 @@ class CylinderImpostorRenderer(ShaderBaseRenderer):
         vertices = np.repeat(bounds, 2, axis=1).astype(np.float32)
         radii = np.repeat(radii, 4, axis=0).astype(np.float32)
         colors = np.repeat(colors, 4, axis=0).astype(np.uint8)
-        
+        directions = np.repeat(bounds[:, 1] - bounds[:, 0], 4, axis=0).astype(np.float32)
 
         mapping = np.tile([-1, -1, # Black corner
                            1, -1, # Red Corner
                            1, 1,  # Yellow corner
                            -1, 1],
                           self.n_cylinders).astype(np.float32)
-        print vertices        
-        print mapping
         
         self._verts_vbo = VertexBuffer(vertices,GL_DYNAMIC_DRAW)
         self._color_vbo = VertexBuffer(colors,GL_DYNAMIC_DRAW)
         self._mapping_vbo = VertexBuffer(mapping,GL_DYNAMIC_DRAW)
         #self._centers_vbo = VertexBuffer(vertices,GL_DYNAMIC_DRAW)
         self._radii_vbo = VertexBuffer(radii,GL_DYNAMIC_DRAW)
+        self._directions_vbo = VertexBuffer(directions, GL_DYNAMIC_DRAW)
+        
 
     def setup_shader(self):
         glUseProgram(self.shader)
         set_uniform(self.shader, 'camera_mat', 'mat4fv',
                     self.viewer.camera.matrix)
         
+        set_uniform(self.shader, 'camera_rotation', 'mat4fv',
+                    self.viewer.camera._get_rotation_matrix())
         
         set_uniform(self.shader, 'projection_mat', 'mat4fv',
                     self.viewer.camera.projection)
+        
         set_uniform(self.shader, 'mvproj', 'mat4fv',
-                    self.viewer.camera.projection)
+                    np.dot(self.viewer.camera.projection, self.viewer.camera.matrix))
         
         set_uniform(self.shader, 'light_dir', '3f', self.ldir[:3])
         
@@ -69,16 +72,17 @@ class CylinderImpostorRenderer(ShaderBaseRenderer):
         
         at_mapping = glGetAttribLocation(self.shader,
                                          "at_mapping")
-        #at_cylinder_radius = glGetAttribLocation(self.shader,
-        #                                       "cylinder_radius")
+        #at_cylinder_direction = glGetAttribLocation(self.shader,
+        #                                       "at_cylinder_direction")
         
         glEnableVertexAttribArray(at_mapping)        
-        #glEnableVertexAttribArray(at_cylinder_radius)
+        #glEnableVertexAttribArray(at_cylinder_direction)
         
         glEnableClientState(GL_VERTEX_ARRAY)
         self._verts_vbo.bind_vertexes(3, GL_FLOAT)
         
         self._mapping_vbo.bind_attrib(at_mapping, 2, GL_FLOAT)
+        #self._directions_vbo.bind_attrib(at_cylinder_direction, 3, GL_FLOAT)
         #self._radii_vbo.bind_attrib(at_cylinder_radius, 1, GL_FLOAT)
 
         glEnableClientState(GL_COLOR_ARRAY)
@@ -88,11 +92,11 @@ class CylinderImpostorRenderer(ShaderBaseRenderer):
         
         self._verts_vbo.unbind()
         self._mapping_vbo.unbind()
-        self._radii_vbo.unbind()
+        self._directions_vbo.unbind()
         self._color_vbo.unbind()
         
         glDisableVertexAttribArray(at_mapping)        
-        #glDisableVertexAttribArray(at_cylinder_radius)
+        #glDisableVertexAttribArray(at_cylinder_direction)
 
         glDisableClientState(GL_VERTEX_ARRAY)
         glDisableClientState(GL_COLOR_ARRAY)
--- chemlab/graphics/renderers/shaders/cylinderimp.vert
+++ chemlab/graphics/renderers/shaders/cylinderimp.vert
@@ -3,6 +3,7 @@
 uniform mat4 mvproj;
 uniform mat4 camera_mat;
 uniform mat4 projection_mat;
+uniform mat4 camera_rotation;
 
 // Camera in world coordinates
 uniform vec3 camera; 
@@ -20,6 +21,7 @@ attribute float cylinder_radius;
 attribute vec2 at_mapping;
 //varying vec2 mapping;
 
+attribute vec3 at_cylinder_direction;
 //attribute vec3 at_cylinder_vert;
 //varying vec3 cylinder_vert;
 
@@ -29,22 +31,29 @@ attribute vec2 at_mapping;
 void main()
 {
   //mapping = at_mapping;
-    
+
     //sphere_center = vec3(
     //    camera_mat*vec4(at_sphere_center, 1.0)).xyz;
     
     //sphere_radius = at_sphere_radius;
     vec4 actual_vertex = gl_Vertex;
     
-    // First rotate the points
-    actual_vertex = camera_mat * actual_vertex;
+    // Project the points on the plane (now this is in clip space)
+    actual_vertex = mvproj * actual_vertex;
+
+    // calculate the cylinder axis in camera space.  we want the
+    // axis of our billboard, so we need to project this guy on the z
+    // plane
+    vec3 axis = normalize(mat3(camera_rotation) * at_cylinder_direction);
     
-    actual_vertex = vec4(
-			 actual_vertex.x + (at_mapping.x*0.5 + 0.5),
+    // Side development of our cylinder billboard
+    vec3 disp = normalize(cross(actual_vertex.xyz, axis));
+    
+    actual_vertex = vec4(actual_vertex.x + 0.5*at_mapping.x,
 			 actual_vertex.y,
 			 actual_vertex.z,
 			 actual_vertex.w);
     
-    gl_Position = projection_mat * actual_vertex;       
-    gl_FrontColor = vec4(at_mapping*0.5 + 0.5, 0.0, 1.0);
+    gl_Position = actual_vertex;       
+    gl_FrontColor = vec4(actual_vertex.x, actual_vertex.y, abs(actual_vertex.z), 1.0);
 }
