d3e59812d9a1adfbeaa0428e550f1c28a238096a
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-03-09 17:03:34 -0700

    Created a periodic boundary conditions module

--- chemlab/utils/pbc.py
+++ chemlab/utils/pbc.py
@@ -68,13 +68,15 @@ def noperiodic(r_array, periodic, reference=None):
     return r_array
 
 
-def subtract_vectors(vec_a, vec_b, periodic):
+def subtract_vectors(a, b, periodic):
     '''Returns the difference of the points vec_a - vec_b subject 
        to the periodic boundary conditions.
 
     '''
-    moved = noperiodic(np.array([vec_b, vec_a]), periodic)
-    return vec_a - vec_b
+    r = a - b
+    delta = np.abs(r)
+    sign = np.sign(r)
+    return np.where(delta > 0.5 * periodic, sign * (periodic - delta), r)
 
 
 def add_vectors(vec_a, vec_b, periodic):
@@ -84,3 +86,43 @@ def add_vectors(vec_a, vec_b, periodic):
     '''
     moved = noperiodic(np.array([vec_a, vec_b]), periodic)
     return vec_a + vec_b
+
+
+def distance_matrix(coordinates, periodic):
+    '''
+
+    '''
+    a = coordinates
+    b = coordinates[:, np.newaxis]
+    return periodic_distance(a, b, periodic)
+
+def periodic_distance(a, b, periodic):
+    '''Periodic distance between two arrays. Periodic is a 3
+    dimensional array containing the 3 box sizes.
+
+    '''
+    delta = np.abs(a - b)
+    delta = np.where(delta > 0.5 * periodic, periodic - delta, delta)
+    return np.sqrt((delta ** 2).sum(axis=-1))
+
+
+def geometric_center(coords, periodic):
+    '''Geometric center taking into account periodic boundaries'''
+    max_vals = periodic
+    theta = 2*np.pi*(coords / max_vals)
+    eps = np.cos(theta) * max_vals/(2*np.pi)
+    zeta = np.sin(theta) * max_vals/(2*np.pi)
+
+    eps_avg = eps.sum(axis=0)
+    zeta_avg = zeta.sum(axis=0)
+    theta_avg = np.arctan2(-zeta_avg, -eps_avg) + np.pi
+
+    return theta_avg * max_vals /(2*np.pi)
+
+
+def radius_of_gyration(coords, periodic):
+    '''Calculate the square root of the mean distance squared from the center of gravity.
+
+    '''
+    gc = geometric_center(coords, periodic)
+    return (periodic_distance(coords, gc, periodic)**2).sum()/len(coords)
