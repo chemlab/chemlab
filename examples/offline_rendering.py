1841758de624a4ba3bc7e7eb83dc258b74f1174b
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-08-10 09:31:35 -0700

    Update example

--- examples/offline_rendering.py
+++ examples/offline_rendering.py
@@ -10,19 +10,19 @@ db = CirDB()
 
 # Prepare the viewer
 v = QtViewer()
+v.widget.initializeGL()
 v.add_post_processing(SSAOEffect, kernel_size=128, kernel_radius=1.0)
 v.add_post_processing(FXAAEffect)
 
 for compound in compounds:
     mol = db.get("molecule", compound)
     rend = v.add_renderer(AtomRenderer, mol.r_array, mol.type_array)
-    
+
     v.widget.camera.autozoom(mol.r_array)
     # Give some extra zoom
     v.widget.camera.mouse_zoom(1.0)
-    
+
     v.widget.toimage(300, 300).save(compound + '.png')
-    
+
     # Cleanup
     v.remove_renderer(rend)
-
