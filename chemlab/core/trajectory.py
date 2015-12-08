78ddb393bbe19d26f496291b75f4391c021f8939
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-12-08 14:32:02 -0800

    Added new trajectory backend

---
+++ chemlab/core/trajectory.py
@@ -0,0 +1,31 @@
+"""Trajectory class for chemlab"""
+import numpy as np
+
+class Trajectory(object):
+    
+    def __init__(self, coords, t=None, boxes=None):
+        self._data = {}
+        self.nframes = len(coords)
+        self._ignored = set(['_ignored'] + self.__dict__.keys())
+         
+        self.coords = coords
+        self.t = t if t is not None else np.arange(0, self.nframes) 
+        self.boxes = boxes if boxes is not None else [None] * self.nframes
+
+    def __setitem__(self, name, value):
+        setattr(self, name, value)
+    
+    def __getitem__(self, name):
+        return getattr(self, name)
+        
+    def at(self, frame, attributes=None):
+        return {k: self[k][frame] for k in set(self.__dict__.keys()) - self._ignored}
+    
+    def map(self, func, attributes=None):
+        for i in range(self.nframes):
+            if attributes is None:
+                yield func({k: self[k][i] for k in set(self.__dict__.keys()) - self._ignored})
+            if isinstance(attributes, str):
+                yield func(self[attributes][i])
+            if isinstance(attributes, list):
+                yield func({a: self[a][i] for a in attrbutes})
---
+++ tests/test_trajectory.py
@@ -0,0 +1,63 @@
+import numpy as np
+from testtools import npeq_
+from chemlab.core import Trajectory, System
+np.random.seed(42)
+
+def test_init():    
+    coords = [np.random.rand(10, 3) for i in range(10)]
+    t = np.arange(0, 10, 0.1)
+    
+    traj = Trajectory(coords, t)
+    npeq_(traj.at(1)["coords"], coords[1]) 
+
+
+def test_attributes():
+    coords = [np.random.rand(10, 3) for i in range(10)]
+    t = np.arange(0, 10, 0.1)
+    traj = Trajectory(coords, t)
+    system = System.from_arrays(r_array=coords[0])
+    
+    system.update(traj.at(1))    
+    npeq_(system.r_array, coords[1])
+     
+def test_map():
+    coords = [np.random.rand(10, 3) for i in range(10)]
+    t = np.arange(0, 10, 0.1)
+    traj = Trajectory(coords, t)
+    
+    summed_coords = traj.map(np.sum, attributes="coords")
+    print np.array(list(summed_coords))
+
+def test_rolling_func():
+    pass
+    # summed_coords = traj.rolling(np.sum, attributes="coords")
+
+
+def test_offload():
+    from chemlab.io import datafile
+
+    # We offload and retrieve a dask array
+    traj = datafile("tests/data/trajout.xtc").read("trajectory", rootdir="ds")
+    
+# class DataStore:
+#     
+#     def __init__(self, rootdir):
+#         self.rootdir = rootdir
+#         if not os.exists(rootdir):
+#             os.mkdir(rootdir)
+#     
+#     def __setitem__(self, attr, value):
+#         bcolz.open()
+# 
+#     
+# def test_out_of_core():
+#     ds = DataStore("data/")
+#     ds['coords'] = np.random.rand(10, 100, 3)
+#     old = ds['coords'][0, 0, 0]
+#     ds['coords'][0, 0, 0] = old + 1
+    
+    
+    
+    # ds.empty("bond_orders", 10, 100)
+    
+    
